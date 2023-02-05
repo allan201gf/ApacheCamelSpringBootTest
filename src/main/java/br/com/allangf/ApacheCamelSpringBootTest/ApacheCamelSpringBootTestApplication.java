3e078bbceea26bd40a072038ee75b4f6418e13eb
allan201gf <allan201gf@gmail.com>
2023-02-05 15:15:37 -0300

    First commit

---
+++ src/main/java/br/com/allangf/ApacheCamelSpringBootTest/ApacheCamelSpringBootTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.allangf.ApacheCamelSpringBootTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheCamelSpringBootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheCamelSpringBootTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/allangf/ApacheCamelSpringBootTest/ApacheCamelSpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.allangf.ApacheCamelSpringBootTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheCamelSpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
